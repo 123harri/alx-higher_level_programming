667da2c6e539f657d49111e8b8f8cdb4f8ec65b1
123harri <harrietwanjiku90@gmail.com>
2023-11-30 09:14:03 -0800

    hidden discovery correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
-def print_sorted_names():
+if __name__ == "__main__":
     import hidden_4
 
-    for name in sorted(dir(hidden_4)):
-        if name[:2] != '__':
-            print("{}".format(name))
+    def print_sorted_names():
+        for name in sorted(dir(hidden_4)):
+            if name[:2] != '__':
+                print("{}".format(name))
 
-if __name__ == "__main__":
+    # Call the function when the script is executed directly
     print_sorted_names()
