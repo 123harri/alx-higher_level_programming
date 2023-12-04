03c4921d95471391a7435b33f209abe9d123ced5
123harri <harrietwanjiku90@gmail.com>
2023-12-04 09:54:22 -0800

    more correction

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    """
-    Delete the item at a specific position in a list.
-    If idx is negative or out of range, nothing changes (returns the same list).
-    """
     if 0 <= idx < len(my_list):
         del(my_list[idx])
     return(my_list)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """
-    Replace an element in a list at a specific position without modifying the original list.
-    If idx is negative or out of range, return a copy of the original list.
-    """
-    tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element
-        return(tmp_list)
-    return(my_list)
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(new_list):
+        return new_list
+    new_list[idx] = element
+    return new_list
