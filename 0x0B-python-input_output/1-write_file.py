90e28953f69d08dd4069260624733debd779df6d
123harri <harrietwanjiku90@gmail.com>
2024-01-09 18:59:25 -0800

    correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -16,6 +16,5 @@ def write_file(filename="", text=""):
     Returns:
         int: The number of characters written.
     """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        characters_written = file.write(text)
-        return characters_written + text.count('\n')
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
