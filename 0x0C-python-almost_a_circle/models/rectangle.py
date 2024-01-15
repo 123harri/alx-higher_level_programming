a9e25180c965793be406c618b66259b1311d3219
123harri <harrietwanjiku90@gmail.com>
2024-01-15 10:27:45 -0800

    b

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python3
+
+"""
+Rectangle module containing the Rectangle class.
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class inheriting from Base.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor for Rectangle class.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): X coordinate of the rectangle.
+            y (int): Y coordinate of the rectangle.
+            id (int): If not None, assign it to the id attribute.
+                      Otherwise, increment __nb_objects
+                      and assign the new value to the id attribute.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter for width attribute.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for width attribute.
+
+        Args:
+            value (int): Value to set for width.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter for height attribute.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for height attribute.
+
+        Args:
+            value (int): Value to set for height.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter for x attribute.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter for x attribute.
+
+        Args:
+            value (int): Value to set for x.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter for y attribute.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter for y attribute.
+
+        Args:
+            value (int): Value to set for y.
+        """
+        self.__y = value
+
+
+def validate_integer(self, name, value, eq=True):
+    """Method for validating the value."""
+    if type(value) != int:
+        raise TypeError("{} must be an integer".format(name))
+    if eq and value < 0:
+        raise ValueError("{} must be >= 0".format(name))
+    elif not eq and value <= 0:
+        raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        """
+        Calculate and return the area of the rectangle.
+
+        Returns:
+            int: Area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        Print the Rectangle instance with the character #.
+        """
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle instance.
+
+        Returns:
+            str: String representation of the Rectangle.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
+
+
+def update(self, *args, **kwargs):
+    """Update attributes of the Rectangle
+    instance based on arguments."""
+    if args:
+        self._update(*args)
+    elif kwargs:
+        self._update(**kwargs)
+
+        def _update(self, id=None, width=None, height=None, x=None, y=None):
+            """Internal method that update instance via */**args."""
+            if id is not None:
+                self.id = id
+            if width is not None:
+                self.width = width
+            if height is not None:
+                self.height = height
+            if x is not None:
+                self.x = x
+            if y is not None:
+                self.y = y
