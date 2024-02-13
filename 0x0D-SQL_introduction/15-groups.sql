a90d374197cee50511b43d3bce263241b0ef71ba
123harri <harrietwanjiku90@gmail.com>
2024-02-13 10:38:35 -0800

    a

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Script to list the number of records with the same score in the table second_table
+SELECT score, COUNT(*) AS number_of_records
+FROM second_table
+GROUP BY score
+ORDER BY number_of_records DESC;
