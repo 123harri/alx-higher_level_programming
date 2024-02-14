ec9a7c105db17174b826875741ab8a6baa7cd846
123harri <harrietwanjiku90@gmail.com>
2024-02-14 05:29:40 -0800

    a

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- If hbtn_0d_2 doesn't exist, create the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- If user_0d_2 doesn't exist, create the user and grant SELECT privilege
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant SELECT privilege to user_0d_2 on hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
