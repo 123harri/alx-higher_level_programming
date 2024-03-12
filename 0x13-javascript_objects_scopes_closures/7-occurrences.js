d5c28d89a6e042f95a8aabae60af045c118e99c3
123harri <harrietwanjiku90@gmail.com>
2024-03-12 05:12:14 -0700

    a

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
-  // Use the reduce method to count occurrences
-  let nOccurrences = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (searchElement === list[i]) {
-      nOccurrences++;
-    }
-  }
-  return nOccurrences;
+
+/**
+ * Function to return the number of occurrences of a specified element in a list.
+ * @param {Array} list - The list to search for occurrences.
+ * @param {*} searchElement - The element to count occurrences for.
+ * @returns {number} - The number of occurrences of the searchElement in the list.
+ */
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce(function (count, element) {
+    return (element === searchElement) ? count + 1 : count;
+  }, 0);
 };
