50e15713f6ba4c757a92533c8140e23e527df83e
123harri <harrietwanjiku90@gmail.com>
2024-03-16 20:07:09 -0700

    a

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,57 +1,25 @@
 #!/usr/bin/python3
-"""
-Lists all cities of a given state from the database hbtn_0e_4_usa.
-"""
+""" Lists all cities of a given state from the database hbtn_0e_0_usa. """
 
 import MySQLdb
 import sys
 
-
-def list_cities_by_state(username, password, database, state_name):
-    """
-    Function to list all cities of a given state
-    from the database hbtn_0e_4_usa.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): MySQL database name.
-        state_name (str): Name of the state.
-
-    Returns:
-        None
-    """
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        query = "SELECT cities.id, cities.name FROM cities \
-                 JOIN states ON states.id = cities.state_id \
-                 WHERE states.name = %s \
-                 ORDER BY cities.id"
-        cursor.execute(query, (state_name,))
-        cities = cursor.fetchall()
-        for city in cities:
-            print(city)
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL:", e)
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
-        if 'db' in locals():
-            db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(
-            sys.argv[0]))
-        sys.exit(1)
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
 
-    username, password, database, state_name = sys.argv[1:5]
-    list_cities_by_state(username, password, database, state_name)
+    # Execute SQL query to fetch cities of the given state
+    cur.execute("""SELECT cities.name FROM
+                   cities INNER JOIN states ON states.id=cities.state_id
+                   WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+
+    # Extract city names from the result and print them
+    cities = [row[0] for row in rows]
+    print(", ".join(cities))
+
+    # Close cursor and database connection
+    cur.close()
+    db.close()
