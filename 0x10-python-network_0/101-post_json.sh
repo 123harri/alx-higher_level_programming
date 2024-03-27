9a39c959a35ee6713a4346b6246096247d25929c
123harri <harrietwanjiku90@gmail.com>
2024-03-27 03:30:29 -0700

    a

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-# Sends a JSON POST request to the URL passed as the first argument,
-# with the contents of a file passed as the second argument, and
-# displays the body of the response.
+# Sends a JSON POST request to the URL,with the contents of a file, and displays the body of the response.
 curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
