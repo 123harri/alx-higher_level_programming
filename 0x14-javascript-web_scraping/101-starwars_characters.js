41557e07f287057ea13074a8bad62462d31449c1
123harri <harrietwanjiku90@gmail.com>
2024-04-23 03:49:20 -0700

    a

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,30 +9,30 @@ const id = process.argv[2];
 // Construct the URL for the Star Wars API endpoint
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-// Make a GET request to the specified URL
+// Make an HTTP GET request to the specified URL
 request.get(url, (err, response, body) => {
   // Check for error during the request
   if (err) {
     // Print the error
     console.log(err);
   } else {
-    // Parse the response body
+    // Parse the JSON response body
     const content = JSON.parse(body);
     // Get the list of characters
     const characters = content.characters;
     // Iterate over each character URL
-    for (const characterUrl of characters) {
-      // Make a GET request to fetch the character details
-      request.get(characterUrl, (err, response, body) => {
+    for (const character of characters) {
+      // Make a request to fetch data for the character
+      request.get(character, (err, response, body) => {
         // Check for error during the request
         if (err) {
           // Print the error
           console.log(err);
         } else {
-          // Parse the character details
-          const characterData = JSON.parse(body);
+          // Parse the character data
+          const names = JSON.parse(body);
           // Print the character name
-          console.log(characterData.name);
+          console.log(names.name);
         }
       });
     }
